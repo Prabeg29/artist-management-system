138bc732b38ad5096adc5015c46ba7bf74dc3cb4
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-07-15 09:11:53 +0545

    Complete user sign-in
    - [x] Add validation for signin payload
    - [x] Add route and controller method for signin
    - [x] Add package for jwt
    - [x] Add jwt secret to config
    - [x] Add signin method to service
    - [x] Update type, mapper and controller
    - [x] Send only the email and role as data for token
    
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -3,14 +3,16 @@ import { Router } from 'express';
 import knex from '../database';
 import { validate } from '@middlewares/validator';
 import { AuthService } from '@modules/auth/auth.service';
-import { signupSchema } from '@modules/auth/signup.schema';
 import { tryCatchWrapper } from '@utils/try-catch-wrapper';
 import { AuthController } from '@modules/auth/auth.controller';
+import { signinSchema } from '@modules/auth/validations/signin.schema';
+import { signupSchema } from '@modules/auth/validations/signup.schema';
 import { KnexUserRepository } from '@modules/user/knex-user.repository';
 
 const router: Router = Router();
 const authController = new AuthController(new AuthService(new KnexUserRepository(knex)));
 
 router.post('/signup', validate(signupSchema), tryCatchWrapper(authController.signup));
+router.post('/signin', validate(signinSchema), tryCatchWrapper(authController.signin));
 
 export default router;
