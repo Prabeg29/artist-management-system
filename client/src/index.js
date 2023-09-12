1056502ae05198207c0831ad5e1402cac1592f98
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-09-12 09:27:18 +0545

    [x] - Add react router with routes for signup and error page
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider }  from 'react-router-dom';
+import Signup from './pages/Signup';
+import Page404 from './pages/Page404';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Signup />,
+        errorElement: <Page404 />
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
