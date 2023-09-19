bfbb0949e662404c49665e29c0ce52c1d78b9414
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-09-19 09:20:57 +0545

    [x] - Use react-hook-form
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,172 +1,164 @@
-import { Box, Button, Container, CssBaseline, Grid, MenuItem, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
+import { useForm, Controller } from "react-hook-form";
+import { Box, Button, Container, CssBaseline, Grid, MenuItem, TextField, Typography } from "@mui/material";
 
 const initialUserInfo = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    phone: '',
-    dob: '',
-    gender: 'female',
-    address: '',
-    role: 'artist'
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  phone: "",
+  dob: "",
+  gender: "female",
+  address: "",
+  role: "artist",
 };
 
 const Signup = () => {
-    const [userInfo, setUserInfo] = useState(initialUserInfo);
+  const { control, register, handleSubmit } = useForm({
+    defaultValues: initialUserInfo,
+  });
+  const onSubmit = (data) => console.log(data);
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Sign Up
-          </Typography>
-          <Box component="form" sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  value={userInfo.firstName}
-                  onChange={e => setUserInfo({...userInfo, firstName: e.target.value})}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="lastName"
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  autoFocus
-                  value={userInfo.lastName}
-                  onChange={e => setUserInfo({...userInfo, lastName: e.target.value})}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="email"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email"
-                  autoFocus
-                  value={userInfo.email}
-                  onChange={e => setUserInfo({...userInfo, email: e.target.value})}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField 
-                    name="password" 
-                    required 
-                    fullWidth 
-                    id="password" 
-                    label="Password" 
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="firstName"
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                {...register("firstName", { required: true })}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="lastName"
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                autoFocus
+                {...register("lastName", { required: true })}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="given-name"
+                name="email"
+                fullWidth
+                id="email"
+                label="Email"
+                autoFocus
+                {...register("email", { required: true })}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="password"
+                fullWidth
+                id="password"
+                label="Password"
+                autoFocus
+                {...register("password", { required: true })}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="phone"
+                fullWidth
+                id="phone"
+                label="Phone"
+                autoFocus
+                {...register("phone", { required: true })}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField name="dob" fullWidth id="dob" label="DOB" autoFocus {...register("dob", { required: true })} />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="gender"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    name="gender"
+                    fullWidth
+                    id="gender"
+                    label="Gender"
                     autoFocus
-                    value={userInfo.password}
-                    onChange={e => setUserInfo({...userInfo, password: e.target.value})}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField 
-                    name="phone" 
-                    required 
-                    fullWidth 
-                    id="phone" 
-                    label="Phone" 
-                    autoFocus 
-                    value={userInfo.phone}
-                    onChange={e => setUserInfo({...userInfo, phone: e.target.value})}
-                    />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField 
-                    name="dob" 
-                    required 
-                    fullWidth 
-                    id="dob" 
-                    label="DOB" 
-                    autoFocus 
-                    onChange={e => setUserInfo({...userInfo, dob: e.target.value})}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="gender"
-                  required
-                  fullWidth
-                  id="gender"
-                  label="Gender"
-                  autoFocus
-                  select
-                  value={userInfo.gender}
-                  onChange={e => setUserInfo({...userInfo, gender: e.target.value})}
-                >
-                  <MenuItem value="male">Male</MenuItem>
-                  <MenuItem value="female">Female</MenuItem>
-                  <MenuItem value="other">Other</MenuItem>
-                </TextField>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField 
-                    name="address" 
-                    required 
-                    fullWidth 
-                    id="address" 
-                    label="Address" 
-                    autoFocus 
-                    value={userInfo.address}
-                    onChange={e => setUserInfo({...userInfo, address: e.target.value})}
-                    />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField 
-                    name="role" 
-                    required 
-                    fullWidth 
-                    id="role" 
-                    label="Role" 
-                    autoFocus 
-                    select 
-                    value={userInfo.role}
-                    onChange={e => setUserInfo({...userInfo, role: e.target.value})}
-                    >
-                  <MenuItem value="artist">Artist</MenuItem>
-                  <MenuItem value="artist_manager">Artist Manager</MenuItem>
-                </TextField>
-              </Grid>
+                    select
+                    {...register("gender", { required: true })}
+                  >
+                    <MenuItem value="male">Male</MenuItem>
+                    <MenuItem value="female">Female</MenuItem>
+                    <MenuItem value="other">Other</MenuItem>
+                  </TextField>
+                )}
+              />
             </Grid>
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link to={`/login`} variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="address"
+                fullWidth
+                id="address"
+                label="Address"
+                autoFocus
+                {...register("address", { required: true })}
+              />
             </Grid>
-          </Box>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="role"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    name="role"
+                    fullWidth
+                    id="role"
+                    label="Role"
+                    autoFocus
+                    select
+                    {...register("role", { required: true })}
+                  >
+                    <MenuItem value="artist">Artist</MenuItem>
+                    <MenuItem value="artist_manager">Artist Manager</MenuItem>
+                  </TextField>
+                )}
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link to={`/login`} variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
 };
 
