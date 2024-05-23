f03c15f0e3930494117d313ffb02753690604f04
Prabeg Shakya <shakyaprabeg@gmail.com>
2024-05-23 16:44:50 +0545

    [x] - Update schema imports
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -4,19 +4,18 @@ import { validate } from '../middlewares/validator';
 import { UserService } from '../modules/user/user.service';
 import { tryCatchWrapper } from '../utils/try-catch-wrapper';
 import { AuthController } from '../modules/auth/auth.controller';
-import { ArtistService } from '../modules/artists/artist.service';
-import { signinSchema } from '../modules/auth/validations/signin.schema';
+import { TenantService } from '../modules/tenants/tenant.service';
+import { signinSchema, signupSchema } from '../modules/auth/auth.schema';
 import { KnexUserRepository } from '../modules/user/knex-user.repository';
-import { KnexArtistRepository } from '../modules/artists/knex-artist.repository';
-import { createUserSchema } from '../modules/user/validations/create-user.schema';
+import { KnexTenantRepository } from '../modules/tenants/knex-tenant.repository';
 
 const router: Router = Router();
 const authController = new AuthController(
+  new TenantService(new KnexTenantRepository()),
   new UserService(new KnexUserRepository()),
-  new ArtistService(new KnexArtistRepository()),
 );
 
-router.post('/signup', validate(createUserSchema), tryCatchWrapper(authController.signup));
+router.post('/signup', validate(signupSchema), tryCatchWrapper(authController.signup));
 router.post('/signin', validate(signinSchema), tryCatchWrapper(authController.signin));
 
 export default router;
